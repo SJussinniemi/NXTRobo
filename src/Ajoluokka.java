42568a714b8e23d58cb2999eacebec859e14ba6b
Valtteri Lattu <valtteri1403@student.hamk.fi>
2017-04-06 12:51:52 +0300

    IO

--- src/Ajoluokka.java
+++ src/Ajoluokka.java
@@ -1,7 +1,14 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.MotorPort;
+import lejos.nxt.comm.USB;
+import lejos.nxt.comm.USBConnection;
 
 
 /**
@@ -23,7 +30,32 @@ public class Ajoluokka {
 	/** Et‰isyys, jolta robotin ultra-anturi havaitsee esteen*/
 	public static int MAX_DETECT = 25; // Et‰isyys jolta robotti havaitsee esteen (cm)
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		LCD.drawString("waiting", 0, 0);
+		USBConnection conn = USB.waitForConnection();
+		DataOutputStream dOut = conn.openDataOutputStream();
+		DataInputStream dIn = conn.openDataInputStream();
+		LCD.clear();
+		while (true) 
+		{
+            String b = "Valtsu";
+            try
+            {
+                b = dIn.readUTF();
+            }
+            catch (EOFException e) 
+            {
+                break;
+            }         
+			dOut.writeUTF(b);
+			dOut.flush();
+	        LCD.drawString(b, 8, 0);
+		}
+        dOut.close();
+        dIn.close();
+        conn.close();
+		
 		
 		//Alustetaan oliot
 		Moottori Mot = new Moottori();
@@ -35,7 +67,7 @@ public class Ajoluokka {
 		Thread Sensorit = new Thread(aT);
 
 		// Ohjelma aloittaa suorittamisen napin painalluksen j‰lkeen
-		LCD.drawString("Mr.Robotto", 0, 0);
+	//	LCD.drawString("Mr.Robotto", 0, 0);
 		Button.waitForAnyPress();
 		
 		//K‰ynnistet‰‰n s‰ie viivan- ja esteen haistelua varten
---
+++ src/UltraAnturi.java
@@ -0,0 +1,16 @@
+import lejos.nxt.Button;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.nxt.LCD;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.objectdetection.Feature;
+import lejos.robotics.objectdetection.FeatureDetector;
+import lejos.robotics.objectdetection.FeatureListener;
+import lejos.robotics.objectdetection.RangeFeatureDetector;
+import lejos.util.Delay;
+
+public class UltraAnturi  {
+
+
+}
